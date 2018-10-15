d3317c0e8baa1cac56c7ffc6d11e6ba7a03e4c07
Cheuk Yin Ng <chucksys88@gmail.com>
2018-10-14 23:53:16 -0700

    Add 2 rules
    Really the hackiest way possible to do these things. May need to create
    my own custom parser later.

--- pymarkdownlint/rules.py
+++ pymarkdownlint/rules.py
@@ -1,7 +1,11 @@
 from abc import abstractmethod, ABCMeta
 from pymarkdownlint.options import IntOption
+from bs4 import BeautifulSoup
+import markdown
 
 import re
+
+RE_HEADERS = re.compile('^h[1-6]$')
 
 
 class Rule(object, metaclass=ABCMeta):
@@ -9,6 +13,7 @@ class Rule(object, metaclass=ABCMeta):
     options_spec = []
     id = []
     name = ""
+    error_str = ""
 
     def __init__(self, opts={}):
         self.options = {}
@@ -28,7 +33,14 @@ class Rule(object, metaclass=ABCMeta):
 
 class FileRule(Rule):
     """ Class representing rules that act on an entire file """
-    pass
+
+    @staticmethod
+    def md_to_tree(md):
+        """
+        Converts a string of pure markdown into a bs4 object. Makes parsing much
+        easier, but not necessarily faster.
+        """
+        return BeautifulSoup(markdown.markdown(md), "html.parser")
 
 
 class LineRule(Rule):
@@ -52,25 +64,60 @@ class RuleViolation(object):
         return self.__str__()
 
 
+class HeaderIncrement(FileRule):
+    """Rule: Header levels should only increment 1 level at a time."""
+    name = "header-increment"
+    id = "MD001"
+    error_str = "Headers don't increment"
+
+    def validate(self, lines):
+        soup = self.md_to_tree(lines)
+        old_level = None
+        for header in soup.find_all(RE_HEADERS):
+            level = int(header.name[-1])
+            if old_level and level > old_level + 1:
+                return RuleViolation(self.id, self.error_str)
+            old_level = level
+
+
+class TopLevelHeader(FileRule):
+    """Rule: First header of the file must be h1."""
+    name = "first-header-h1"
+    id = "MD002"
+    options_spec = [IntOption("first-header-level", 1, "Top level header")]
+    error_str = "First header of the file must be top level header"
+
+    def validate(self, lines):
+        soup = self.md_to_tree(lines)
+        top_level = self.options['first-header-level'].value
+        first_header = soup.find(RE_HEADERS)
+        if first_header:
+            first_header_level = int(first_header.name[-1])
+            if top_level != first_header_level:
+                return RuleViolation(self.id, self.error_str)
+
+
 class TrailingWhiteSpace(LineRule):
     """Rule: No line may have trailing whitespace."""
     name = "trailing-whitespace"
     id = "MD009"
+    error_str = "Line has trailing whitespace"
 
     def validate(self, line):
         pattern = re.compile(r"\s$")
         if pattern.search(line):
-            return RuleViolation(self.id, "Line has trailing whitespace")
+            return RuleViolation(self.id, self.error_str)
 
 
 class HardTab(LineRule):
     """Rule: No line may contain tab (\\t) characters."""
     name = "hard-tab"
     id = "MD010"
+    error_str = "Line contains hard tab characters (\\t)"
 
     def validate(self, line):
         if "\t" in line:
-            return RuleViolation(self.id, "Line contains hard tab characters (\\t)")
+            return RuleViolation(self.id, self.error_str)
 
 
 class MaxLineLengthRule(LineRule):
@@ -78,8 +125,9 @@ class MaxLineLengthRule(LineRule):
     name = "max-line-length"
     id = "MD013"
     options_spec = [IntOption('line-length', 80, "Max line length")]
+    error_str = "Line exceeds max length ({0}>{1})"
 
     def validate(self, line):
         max_length = self.options['line-length'].value
         if len(line) > max_length:
-            return RuleViolation(self.id, "Line exceeds max length ({0}>{1})".format(len(line), max_length))
+            return RuleViolation(self.id, self.error_str.format(len(line), max_length))
--- pymarkdownlint/tests/test_rules.py
+++ pymarkdownlint/tests/test_rules.py
@@ -1,39 +1,87 @@
 from pymarkdownlint.tests.base import BaseTestCase
-from pymarkdownlint.rules import MaxLineLengthRule, TrailingWhiteSpace, RuleViolation, HardTab
+import pymarkdownlint.rules as rules
 
 
 class RuleTests(BaseTestCase):
+    def test_header_inc(self):
+        """For MD001."""
+        rule = rules.HeaderIncrement()
+
+        # assert no error
+        violation = rule.validate("""# Hello world
+## Nice day you got there
+# Yoshi
+""")
+        self.assertIsNone(violation)
+
+        # assert error with bad headers
+        violation = rule.validate("""# Hello world
+### Nice day you got there
+# Yoshi
+""")
+        expected_violation = rules.RuleViolation(rule.id, rule.error_str)
+        self.assertEqual(violation, expected_violation)
+
+    def test_top_level_header(self):
+        """For MD002."""
+        rule = rules.TopLevelHeader()
+
+        # assert no error
+        violation = rule.validate("""# Hello wood
+
+Tamagachis
+""")
+        self.assertIsNone(violation)
+
+        # assert error
+        violation = rule.validate("""## hellowood
+
+Tamagachis
+""")
+        expected_violation = rules.RuleViolation(rule.id, rule.error_str)
+        self.assertEqual(violation, expected_violation)
+
+        # no errors if you configured it right
+        rule = rules.TopLevelHeader({'first-header-level': 2})
+        violation = rule.validate("""## hello wood
+
+Tamagachis
+""")
+        self.assertIsNone(violation)
+
     def test_max_line_length(self):
-        rule = MaxLineLengthRule()
+        """For MD013."""
+        rule = rules.MaxLineLengthRule()
 
         # assert no error
         violation = rule.validate("a" * 80)
         self.assertIsNone(violation)
 
         # assert error on line length > 81
-        expected_violation = RuleViolation("MD013", "Line exceeds max length (81>80)")
+        expected_violation = rules.RuleViolation("MD013", "Line exceeds max length (81>80)")
         violation = rule.validate("a" * 81)
         self.assertEqual(violation, expected_violation)
 
         # set line length to 120, and check no violation on length 81
-        rule = MaxLineLengthRule({'line-length': 120})
+        rule = rules.MaxLineLengthRule({'line-length': 120})
         violation = rule.validate("a" * 81)
         self.assertIsNone(violation)
 
         # assert raise on 121
-        expected_violation = RuleViolation("MD013", "Line exceeds max length (121>120)")
+        expected_violation = rules.RuleViolation("MD013", "Line exceeds max length (121>120)")
         violation = rule.validate("a" * 121)
         self.assertEqual(violation, expected_violation)
 
     def test_trailing_whitespace(self):
-        rule = TrailingWhiteSpace()
+        """For MD009."""
+        rule = rules.TrailingWhiteSpace()
 
         # assert no error
         violation = rule.validate("a")
         self.assertIsNone(violation)
 
         # trailing space
-        expected_violation = RuleViolation("MD009", "Line has trailing whitespace")
+        expected_violation = rules.RuleViolation(rule.id, rule.error_str)
         violation = rule.validate("a ")
         self.assertEqual(violation, expected_violation)
 
@@ -42,13 +90,14 @@ class RuleTests(BaseTestCase):
         self.assertEqual(violation, expected_violation)
 
     def test_hard_tabs(self):
-        rule = HardTab()
+        """For MD010."""
+        rule = rules.HardTab()
 
         # assert no error
         violation = rule.validate("This is a test")
         self.assertIsNone(violation)
 
         # contains hard tab
-        expected_violation = RuleViolation("MD010", "Line contains hard tab characters (\\t)")
+        expected_violation = rules.RuleViolation(rule.id, rule.error_str)
         violation = rule.validate("This is a\ttest")
         self.assertEqual(violation, expected_violation)
